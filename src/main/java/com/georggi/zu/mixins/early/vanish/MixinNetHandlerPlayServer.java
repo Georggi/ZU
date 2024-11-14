65d997cd0affda4d07379b3ce9b5f679fb6db13f
Yurii Shnitkovskyi <bmp2558@gmail.com>
2024-11-14 22:06:22 +0200

    Move forgeplayer fix to systemchatmsghidden

--- src/main/java/com/georggi/zu/mixins/early/vanish/MixinNetHandlerPlayServer.java
+++ src/main/java/com/georggi/zu/mixins/early/vanish/MixinNetHandlerPlayServer.java
@@ -2,7 +2,6 @@ package com.georggi.zu.mixins.early.vanish;
 
 import static com.georggi.zu.util.Util.systemChatMsgHidden;
 
-import net.minecraft.command.CommandException;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.server.management.ServerConfigurationManager;
 import net.minecraft.util.IChatComponent;
@@ -21,13 +20,8 @@ public class MixinNetHandlerPlayServer {
             target = "Lnet/minecraft/server/management/ServerConfigurationManager;sendChatMsg(Lnet/minecraft/util/IChatComponent;)V"))
     public void onDisconnect_sendChatMsg(ServerConfigurationManager scm, IChatComponent component) {
         NetHandlerPlayServer nps = (NetHandlerPlayServer) (Object) this;
-
-        try {
-            if (!systemChatMsgHidden(nps.playerEntity)) {
-                scm.sendChatMsg(component);
-            }
-        } catch (CommandException ignored) {
-            // In the meantime, fix '<Player> left the server' without ever joining issue
+        if (!systemChatMsgHidden(nps.playerEntity)) {
+            scm.sendChatMsg(component);
         }
     }
 }
